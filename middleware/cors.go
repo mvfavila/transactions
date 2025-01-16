6b36ce6f53d120f1f3efa3754049d5a19ff56f44
mvfavila <mvfavila@gmail.com>
2025-01-16 20:46:46 -0300

    [#19] Add middleware

---
+++ middleware/cors.go
@@ -0,0 +1,15 @@
+package middleware
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func Cors() gin.HandlerFunc {
+	return cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+	})
+}
